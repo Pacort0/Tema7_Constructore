054f6c2c6bc53af6938a83ad5d204841908718cb
frodriguez <frodriguez@106-24.info2>
2023-03-21 12:45:06 +0100

    ej3

---
+++ TEMA7_2/src/ejercicio03/Fecha.java
@@ -0,0 +1,47 @@
+package ejercicio03;
+
+public class Fecha {
+	private int dia;
+	private int mes;
+	private int anio;
+
+	public Fecha() {
+
+	}
+
+	public Fecha(int dia, int mes, int anio) {
+		this.dia = dia;
+		this.mes = mes;
+		this.anio = anio;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		if (dia > 0 && dia <= 31) {
+			if(getMes() != 2 && dia <= 28) {
+				
+			}
+			this.dia = dia;
+		}
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+}
