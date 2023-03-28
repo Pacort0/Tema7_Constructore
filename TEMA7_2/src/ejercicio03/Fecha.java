cfbf47820adbfc483f96ae900361abadcee13e20
frodriguez <frodriguez@106-24.info2>
2023-03-28 10:24:09 +0200

    ejercicio 3 bueno

--- TEMA7_2/src/ejercicio03/Fecha.java
+++ TEMA7_2/src/ejercicio03/Fecha.java
@@ -20,20 +20,8 @@ public class Fecha {
 	}
 
 	public void setDia(int dia) {
-		if (dia > 0 && dia <= 31) {
-			if (getMes() == 2 && dia <= 28) {
-				this.dia = dia;
-			}
-			else if ((getMes() == 1 || getMes() == 3 || getMes() == 5 || getMes() == 7 || getMes() == 8 || getMes() == 10
-					|| getMes() == 12) && dia <= 31) {
-				this.dia = dia;
-			}
-			else {
-				this.dia =  dia;
-			}
-		}
-		else {
-			System.err.println("Día introducido incorrecto.");
+		if (fechaCorrecta()) {
+			this.dia = dia;
 		}
 	}
 
@@ -42,11 +30,8 @@ public class Fecha {
 	}
 
 	public void setMes(int mes) {
-		if(mes > 0 && mes <= 12) {
+		if (fechaCorrecta()) {
 			this.mes = mes;
-		}
-		else {
-			System.err.println("Mes introducido incorrecto.");
 		}
 	}
 
@@ -55,24 +40,47 @@ public class Fecha {
 	}
 
 	public void setAnio(int anio) {
-		if(anio > 1908 && anio <= 2023) {
+		if (fechaCorrecta()) {
 			this.anio = anio;
 		}
-		else {
-			System.err.println("Año introducido incorrecto.");
-		}
 	}
-	
+
 	public boolean esBisiesto() {
 		boolean bisiesto;
-		if(getAnio() % 4 == 0 || (getAnio() % 100 == 0 && getAnio() % 400 == 0)) { //Si además es divisible entre 100
-					bisiesto = true; //Si es divisible entre 100 y 400 es bisiesto
-				}
-				else {
-					bisiesto = false; //Si no, no lo es
-				}
-		
+		if (getAnio() % 4 == 0 || (getAnio() % 100 == 0 && getAnio() % 400 == 0)) { // Si además es divisible entre 100
+			bisiesto = true; // Si es divisible entre 100 y 400 es bisiesto
+		} else {
+			bisiesto = false; // Si no, no lo es
+		}
+
 		return bisiesto;
 	}
-	
+
+	public boolean fechaCorrecta() {
+		boolean correcta = false;
+
+		if (getDia() > 0 && getDia() <= 31) {
+			if (getMes() == 2 && getDia() <= 28) {
+				correcta = true;
+			}
+			if (getMes() == 2 && getDia() == 29 && !esBisiesto()) {
+				correcta = false;
+			} else if ((getMes() == 1 || getMes() == 3 || getMes() == 5 || getMes() == 7 || getMes() == 8
+					|| getMes() == 10 || getMes() == 12) && getDia() <= 31) {
+				correcta = true;
+			} else {
+				correcta = true;
+			}
+		}
+
+		if (getMes() <= 0 || getMes() > 12) {
+			correcta = false;
+		}
+
+		if (getAnio() < 0 || getAnio() > 2023) {
+			correcta = false;
+		}
+
+		return correcta;
+	}
 }
--- TEMA7_2/src/ejercicio03/Main03.java
+++ TEMA7_2/src/ejercicio03/Main03.java
@@ -1,9 +1,27 @@
 package ejercicio03;
 
+import java.util.Scanner;
+
 public class Main03 {
 
 	public static void main(String[] args) {
+		Fecha primera = new Fecha(5, 3, 1996);
+
+		Scanner sc = new Scanner(System.in);
+
+if (!primera.fechaCorrecta()) {
+			System.out.println("La fecha introducida es incorrecta.");
+		} else {
+			System.out.println("La fecha introducida es correcta.");
+			
+			if (primera.esBisiesto()) {
+				System.out.println("El año introducido es bisiesto.");
+			} else {
+				System.out.println("El año introducido no es bisiesto");
+			}
+		}
 		
+		sc.close();
 	}
 
 }
