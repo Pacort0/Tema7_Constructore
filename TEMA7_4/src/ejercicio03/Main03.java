f214d91d1e4e3e8ed81c50420feed9a05312e588
Admin <Admin@10.100.227.78>
2023-04-13 20:54:29 +0200

    comienzo ejercicio 3 CRUD

---
+++ TEMA7_4/src/ejercicio03/Main03.java
@@ -0,0 +1,9 @@
+package ejercicio03;
+
+public class Main03 {
+
+	public static void main(String[] args) {
+		Pizza obj = new Pizza();
+	}
+
+}
---
+++ TEMA7_4/src/ejercicio03/Pizza.java
@@ -0,0 +1,70 @@
+package ejercicio03;
+
+public class Pizza {
+	enum Tamanio{
+		Mediana, Familiar
+	}
+	enum Tipo{
+		Margarita, CuatroQuesos, Funghi
+	}
+	enum Estado{
+		Pedida, Servida
+	}
+	private int codigo = 0;
+	private Tamanio tamanio = Tamanio.Mediana;
+	private Tipo tipo = Tipo.Margarita;
+	private Estado estado = Estado.Pedida;
+	
+	public Pizza() {
+	}
+
+	public Pizza(int codigo, Tamanio tamanio, Tipo tipo, Estado estado) {
+		this.codigo = codigo;
+		this.tamanio = tamanio;
+		this.tipo = tipo;
+		this.estado = estado;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public Tamanio getTamanio() {
+		return tamanio;
+	}
+
+	public void setTamanio(Tamanio tamanio) {
+		this.tamanio = tamanio;
+	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Pizza [codigo=" + codigo + ", tamanio=" + tamanio + ", tipo=" + tipo + ", estado=" + estado + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
